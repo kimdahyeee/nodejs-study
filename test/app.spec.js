2bc60c2337069a6fe37b119d4832e7477f4760cb
김다혜 <dahye432111@gmail.com>
2020-08-30 23:25:55 +0900

    /users API test code 구현

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,13 +1,34 @@
 const app = require('../app.js');
+const should = require('should')
 const request = require('supertest');
 
 describe('GET /users는 ', () => {
-    it('...', (done) => {
-        request(app)
-            .get('/users')
-            .end((err, res) => {
-                console.log(res.body);
-                done();
-            })
+    describe('성공 시', () => {
+        it('유저 객체를 담은 배열로 응답한다 ', (done) => {
+            request(app)
+                .get('/users')
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                })
+        });
+
+        it('최대 limit 갯수만큼 응답한다 ', (done) => {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2);
+                    done();
+                })
+        });
+    });
+
+    describe('실패 시', () => {
+        it('limit이 숫자형이 아니면 400을 응답한다', (done) => {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done)
+        })
     })
 })
