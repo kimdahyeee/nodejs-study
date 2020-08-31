8021ee7c05c9b0db114da9166e522f2cbb8d2173
김다혜 <dahye432111@gmail.com>
2020-08-31 23:39:02 +0900

    refactor: 테스트 환경 개선
    - 테스트 코드 구동 시 불필요한 로그 제외하기 위해 Node_env 설정 test 로 추가
    - 테스트 코드 실행 시에 서버 구동이 되지 않도록 www.js 파일로 서버 구동 로직 이동

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ var express = require('express');
 var morgan = require('morgan')
 var app = express();
 var user = require('./api/user')
-const port = 3000;
 
-app.use(morgan('dev'))
+if(process.env.NODE_ENV !== 'test') {
+    app.use(morgan('dev'))
+}
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/users', user);
-
-app.listen(port, function () {
-    console.log(`connected ${port} port !`);
-});
 
 module.exports = app
